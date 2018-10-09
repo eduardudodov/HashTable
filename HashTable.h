dfef8edbaedeaa27b84dcb575f113ef9c286c008
Edward <e.udodov@g.nsu.ru>
2018-10-10 00:37:29 +0700

    added insert (+test) added clear  (+test) added erase (without test)

--- HashTable.h
+++ HashTable.h
@@ -6,9 +6,12 @@
 #define HASHTABLE_HASHTABLE_H
 
 #include <string>
+#include <iostream>
+#include <utility>
 #include "stdio.h"
 #include "value.h"
 #include "vector"
+
 #define MAP std::vector<std::vector<std::pair<const Key, const TYPE>>>
 
 typedef unsigned long Hash;
@@ -19,18 +22,35 @@ private:
 
     std::pair<unsigned int, unsigned int> tableSize; //first -- in all, second -- now
     MAP table;
-public:
 
-    Hash hashCalculate(const Key& k);
+public:
+    Hash hashCalculate(const Key &k);
+
     HashTable();
+
     void swap(HashTable &b);
+
     bool insert(const Key &k, const TYPE &v);
+
     void resizeHashTable();
+
+    MAP getTable();
+
+    void clear();
+
+    bool erase(const Key &k);
+
+    bool contains(const Key &k) const;
 
 };
 
 template<class TYPE>
-HashTable<TYPE>::HashTable(){
+MAP HashTable<TYPE>::getTable() {
+    return table;
+}
+
+template<class TYPE>
+HashTable<TYPE>::HashTable() {
     tableSize.first = 10;
     tableSize.second = 0;
     table.resize(tableSize.first);
@@ -47,7 +67,7 @@ template<class TYPE>
 bool HashTable<TYPE>::insert(const Key &k, const TYPE &v) {
     Hash hashV = hashCalculate(k);
     this->table[hashV].push_back(make_pair(k, v));
-    if(++tableSize.second > tableSize.first/2)
+    if (++tableSize.second > tableSize.first / 2)
         resizeHashTable();
     return true;
 }
@@ -55,7 +75,7 @@ bool HashTable<TYPE>::insert(const Key &k, const TYPE &v) {
 template<class TYPE>
 Hash HashTable<TYPE>::hashCalculate(const Key &key) {
     Hash hash = 137;
-    for(char i : key){
+    for (char i : key) {
         hash = i + (hash << 6) + (hash << 16) - hash;
     }
     return hash % tableSize.first;
@@ -63,11 +83,51 @@ Hash HashTable<TYPE>::hashCalculate(const Key &key) {
 
 template<class TYPE>
 void HashTable<TYPE>::resizeHashTable() {
-    tableSize.first*=2;
-    MAP newTable;
-    newTable.resize(tableSize.first);
+    tableSize.first *= 2;
+    MAP oldMap = this->table;
+    table.clear();
+    table.resize(tableSize.first);
+    for (int i = tableSize.first / 2 - 1; i >= 0; --i) {
+        while (!oldMap[i].empty()) {
+            this->table[hashCalculate(oldMap[i].back().first)].push_back(\
+                   make_pair(oldMap[i].back().first, oldMap[i].back().second));
+            oldMap[i].pop_back();
+        }
+        oldMap.pop_back();
+    }
+}
 
+template<class TYPE>
+void HashTable<TYPE>::clear() {
+    this->table.clear();
+    tableSize.first = 10;
+    table.resize(tableSize.first);
+    tableSize.second = 0;
+}
 
+template<class TYPE>
+bool HashTable<TYPE>::erase(const Key &k) {
+    Hash hashKey = hashCalculate(k);
+    if (table[hashKey].size() == 0) return false;
+
+    if (table[hashKey].size() == 1) {
+        if (table[hashKey].back().first != k)
+            table[hashKey].pop_back();
+        return true;
+    } else {
+        for (int i = 0; i < table[hashKey].size(); ++i) {
+            if (table[hashKey][i].first == k) {
+                table[hashKey].erase(table[hashKey].begin() + i);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+template<class TYPE>
+bool HashTable<TYPE>::contains(const Key &k) const {
+    return false;
 }
 
 #endif //HASHTABLE_HASHTABLE_H
--- testing.cpp
+++ testing.cpp
@@ -1,18 +1,61 @@
 #include "HashTable.h"
 #include "value.h"
 #include "gtest/gtest.h"
+#include <cstdlib>
+#define N 1000
 
-TEST(hashTableTest, insertTest){
-    Value v[5];
-    v[0].edit("Andrey", 12, 110);
-    v[1].edit("Andrey", 23, 36);
-    v[2].edit("Andrey", 123, 163410);
-    v[3].edit("Andrey", 53, 113460);
-    v[4].edit("Andrey", 57, 11340);
+Key randomString(size_t length) {
+    auto randchar = []() -> char {
+        const char charset[] =
+                "0123456789"
+                "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                "abcdefghijklmnopqrstuvwxyz";
+        const size_t maxIndex = (sizeof(charset) - 1);
+        return charset[rand() % maxIndex];
+    };
+    Key str(length, 0);
+    std::generate_n(str.begin(), length, randchar);
+    return str;
+}
+
+HashTable<Value> createRandHashTable(){
+    srand(static_cast<unsigned int>(time(NULL)));
+    Value v[N];
+    for(int i = 0; i < N; ++i )
+        v[i].edit(randomString(10), rand()%3, rand()%5);
 
     HashTable<Value> h_0;
-    for(int i = 0; i < 5; ++i){
+    for(int i = 0; i < N; ++i){
         h_0.insert(v[i].getName(), v[i]);
     }
+    return h_0;
+}
 
+TEST(hashTableTest, insertTest){
+
+    HashTable<Value> h_0 = createRandHashTable();
+    for(int i = 0; i < N; ++i){
+        for(int j = 0; j < h_0.getTable()[i].size(); ++j ) {
+            EXPECT_EQ(i, h_0.hashCalculate(h_0.getTable()[i][j].first));
+        }
+    }
+
+}
+
+TEST(hashTableTest, clearTest){
+    HashTable<Value> h_0 = createRandHashTable();
+    h_0.clear();
+    int cnt = 0;
+    for(int i = 0; i < h_0.getTable().size(); ++i){
+        for(int j = 0; j < h_0.getTable()[i].size(); ++i) {
+            ++cnt;
+        }
+    }
+    EXPECT_EQ(cnt, 0);
+}
+
+TEST(hashTableTest, eraseTest){
+    HashTable<Value> h_0 = createRandHashTable();
+   // while()
+   // h_0.getTable()[rand()%N][0]
 }
